ba0b2ebdb7ba16c101026ef042ea3f96c74be63b
Ece Yücesoy <82278234+eceyucesoy@users.noreply.github.com>
2022-12-21 23:13:43 +0300

    Create edit.php

---
+++ edit.php
@@ -0,0 +1,17 @@
+<?php
+include "db.php";
+
+if($_POST){
+    $id= $_POST["editid"];
+    $sql = "SELECT * FROM task WHERE task_id=$id ";
+    $result= mysqli_query($db, $sql);
+    $response=array();
+    while ($row=mysqli_fetch_assoc($result)){
+        $response=$row;
+    }
+    echo json_encode($response);
+
+}
+
+
+?>
