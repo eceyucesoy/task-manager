63be3ff84fa96164d6845ac84dc67efe26a4bc1d
Ece Yücesoy <82278234+eceyucesoy@users.noreply.github.com>
2022-12-21 23:10:25 +0300

    Create statusDN.php

---
+++ statusDN.php
@@ -0,0 +1,13 @@
+<?php
+include "db.php";
+
+if($_POST){
+    $id2= $_POST["draggableId"];
+
+    $sql ="UPDATE task SET status='Done' WHERE task_id= '$id2'";
+    $result = mysqli_query($db, $sql);
+}
+?>
+
+
+
